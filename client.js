97a449ac2f4f65395c87fde12d4b9cad8d431271
Viktor Kronvall <viktor.kronvall@gmail.com>
2014-01-31 13:07:33 +0100

    Usernames, rooms

--- client.js
+++ client.js
@@ -1,6 +1,75 @@
 (function (window, undefined) {
+	var id = document.getElementById.bind(document);
+	var qsel = document.querySelectorAll.bind(document);
+	var create = document.createElement.bind(document);
+	var tnode = document.createTextNode.bind(document);
+
+	var joinedRoom = false;
+	var username = '';
+
 	var socket = io.connect('http://localhost');
-	socket.emit('message', {
-		body: "My awesome message goes here!"
+
+	function prependMesage(message, element) {
+		var li = create('li');
+		var text = tnode(message.user+': '+message.body);
+		li.appendChild(text);
+		var ul = element;
+		ul.insertBefore(li,ul.firstChild);
+	}
+
+	socket.on('message', function(data) {
+		prependMesage(data, id('messages'));
 	});
+
+	socket.on('room message', function(data) {
+		prependMesage(data, id('room-messages'));
+	});
+
+	var userForm = id('user-form');
+	userForm.addEventListener('submit', function(event) {
+		event.preventDefault();
+		var field = id('user-field');
+		username = field.value;
+		field.value = '';
+		id('main').removeAttribute('hidden');
+		id('username').setAttribute('hidden','hidden');
+	});
+
+	var messageForm = id('message-form');
+	messageForm.addEventListener('submit', function(event) {
+		event.preventDefault();
+		var field = id('message-field');
+		var messageText = field.value;
+		field.value = '';
+		socket.emit('message', {
+			body: messageText,
+			user: username
+		});
+	});
+
+	var roomForm = id('room-message-form');
+	roomForm.addEventListener('submit', function(event) {
+		event.preventDefault();
+		var field = id('room-message-field');
+		var messageText = field.value;
+		field.value = '';
+		socket.emit('room message', {
+			body: messageText,
+			room: joinedRoom,
+			user: username
+		});
+	});
+
+	var lobbyForm = id('join-lobby-form');
+	lobbyForm.addEventListener('submit', function(event) {
+		event.preventDefault();
+		var field = id('lobby-field');
+		var lobbyName = field.value;
+		field.value = '';
+		socket.emit('join lobby', {
+			lobby: lobbyName
+		});
+		joinedRoom = lobbyName;
+	});
+
 }(window));
--- server.js
+++ server.js
@@ -1,20 +1,49 @@
 var app = require('koa')();
 var send = require('koa-send');
 var serve = require('koa-static');
+var co = require('co');
+
+var mongo = require('co-easymongo')({
+  dbname: 'chattest'
+});
+
+var messages = mongo.collection('messages');
+
 
 app.use(serve('.'));
 
 var server = require('http').Server(app.callback());
 
+
+
 var io = require('socket.io')(server);
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+
   socket.on('message', function(data){
   	console.log('Recieved data', data);
   	io.sockets.emit('message',data);
+    co(function* () {
+      yield messages.save({
+        user: data.user,
+        body: data.body
+      });
+    })();
   });
-  socket.on('disconnect', function(){
-  	
+
+  socket.on('room message', function(data){
+  	console.log('Recieved data', data);
+  	io.sockets.in(data.room).emit('room message',{
+  		body: data.body,
+  		user: data.user
+  	});
   });
+
+  socket.on('join lobby', function(data){
+  	console.log(data);
+  	socket.join(data.lobby);
+  });
+
+  socket.on('disconnect', function(){});
 });
 
 
